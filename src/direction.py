0d7a65b8d20e54b29d02f7dc850e1251b6a994e8
mhawryluk <marcinhawrylukxx@gmail.com>
2022-02-26 22:40:31 +0100

    Implement some Elevator methods
    * implement move, get_direction and update methods
    * add Stay to Direction enum

--- src/direction.py
+++ src/direction.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+
 class Direction(Enum):
     UP = 1
+    STAY = 0
     DOWN = -1
--- src/elevator.py
+++ src/elevator.py
@@ -1,6 +1,22 @@
+from direction import Direction
+
+
 class Elevator:
     def __init__(self):
-        pass
+        self.current_floor = 0
+        self.destination = 0
+
+    def move(self):
+        direction = self.get_direction()
+        self.current_floor += direction
+
+    def get_direction(self):
+        if self.destination > self.current_floor:
+            return Direction.UP
+        elif self.destination < self.current_floor:
+            return Direction.DOWN
+        return Direction.STAY
 
     def update(self, current_floor, destination):
-        pass
+        self.current_floor = current_floor
+        self.destination = destination

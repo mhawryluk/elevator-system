93faf0476fade3376e2619fb514767809937fa52
mhawryluk <marcinhawrylukxx@gmail.com>
2022-02-26 22:29:17 +0100

    Add basic project structure
    * create template for Elevator, ElevatorSystem and Direction
    * create main.py file

---
+++ src/elevator.py
@@ -0,0 +1,6 @@
+class Elevator:
+    def __init__(self):
+        pass
+
+    def update(self, current_floor, destination):
+        pass
---
+++ src/elevatorsystem.py
@@ -0,0 +1,12 @@
+class ElevatorSystem:
+    def __init__(self):
+        pass
+
+    def pickup(self, floor, direction):
+        pass
+
+    def step(self):
+        pass
+
+    def get_status(self):
+        pass

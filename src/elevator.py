0b94aabe039fa9a4248dc1e55b411657ece0810f
mhawryluk <marcinhawrylukxx@gmail.com>
2022-02-26 23:45:11 +0100

    Start implementing a simple system algorithm
    * change elevator destination to a list
    * pop item from list if reached destination
    * keep flag if doors are to be opened
    * add get_destination_count method useful for choosing best elevator option
    * add get_open_doors on a specific floor method
    * implement pickup function

--- src/elevator.py
+++ src/elevator.py
@@ -5,16 +5,24 @@ class Elevator:
     def __init__(self, id):
         self.id = id
         self.current_floor = 0
-        self.destination = 0
+        self.destinations = []
+        self.open_doors = False
 
     def move(self):
+        self.open_doors = False
         direction = self.get_direction()
         self.current_floor += direction
 
+        while self.destinations and self.current_floor == self.destinations[0]:
+            self.open_doors = True
+            self.destinations.pop(0)
+
     def get_direction(self):
-        if self.destination > self.current_floor:
+        if not self.destinations:
+            return Direction.STAY
+        if self.destinations[0] > self.current_floor:
             return Direction.UP
-        elif self.destination < self.current_floor:
+        if self.destinations[0] < self.current_floor:
             return Direction.DOWN
         return Direction.STAY
 
@@ -23,4 +31,10 @@ class Elevator:
         self.destination = destination
 
     def get_status(self):
-        return self.id, self.current_floor, self.destination
+        return self.id, self.current_floor, self.destinations[0]
+
+    def get_destination_count(self):
+        return len(self.destinations)
+
+    def add_destination(self, floor):
+        self.destinations.append(floor)
--- src/elevatorsystem.py
+++ src/elevatorsystem.py
@@ -7,11 +7,26 @@ class ElevatorSystem:
         self.elevators = [Elevator(i) for i in range(elevator_count)]
 
     def pickup(self, floor, direction):
-        pass
+        elevator_pick = min(
+            self.elevators, key=lambda elevator: elevator.get_destination_count())
+
+        elevator_pick.add_destination(floor)
+
+    def choose_floor(self, elevatorId, floor):
+        self.elevators[elevatorId].add_destination(floor)
 
     def step(self):
         for elevator in self.elevators:
             elevator.move()
+    
+    def get_open_doors(self, floor):
+        open_elevators = []
+
+        for elevator in self.elevators:
+            if elevator.current_floor == floor and elevator.open_doors:
+                open_elevators.append(elevator.get_status())
+        
+        return open_elevators
 
     def get_status(self):
         return [elevator.get_status() for elevator in self.elevators]
